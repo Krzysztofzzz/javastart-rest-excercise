a912f91fcd440d5638b01f8e1c7d39b8eb1d5c9e
Krzysztof <krzubrycki@gmail.com>
2022-05-04 14:24:08 +0200

    wip

--- src/main/java/pl/javastart/equipy/components/user/UserRepository.java
+++ src/main/java/pl/javastart/equipy/components/user/UserRepository.java
@@ -1,21 +1,8 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> b075bdc27629da6a3979fb3617d9e69ec56745b4
 package pl.javastart.equipy.components.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-<<<<<<< HEAD
-=======
-=======
-package pl.javastart.equipy;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
->>>>>>> 340ecb7048c0f48fff224acc2070c8b38fb83786
->>>>>>> b075bdc27629da6a3979fb3617d9e69ec56745b4
 public interface UserRepository extends JpaRepository<User, Long> {
 }
--- src/main/java/pl/javastart/equipy/components/user/UserResource.java
+++ src/main/java/pl/javastart/equipy/components/user/UserResource.java
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
 package pl.javastart.equipy.components.user;
-=======
-<<<<<<< HEAD
-package pl.javastart.equipy.components.user;
-=======
-package pl.javastart.equipy;
->>>>>>> 340ecb7048c0f48fff224acc2070c8b38fb83786
->>>>>>> b075bdc27629da6a3979fb3617d9e69ec56745b4
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
