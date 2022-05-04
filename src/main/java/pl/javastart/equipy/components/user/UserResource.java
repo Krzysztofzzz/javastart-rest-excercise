93cb4ba251df3436fd1fdf1669b9792f90092e68
Krzysztof <krzubrycki@gmail.com>
2022-05-04 14:37:52 +0200

    added finding users by lastname

--- src/main/java/pl/javastart/equipy/components/user/UserResource.java
+++ src/main/java/pl/javastart/equipy/components/user/UserResource.java
@@ -2,6 +2,7 @@ package pl.javastart.equipy.components.user;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,7 +17,10 @@ public class UserResource {
     }
 
     @GetMapping("")
-    List<UserDto> findAll() {
-        return userService.findAll();
+    public List<UserDto> findAll(@RequestParam(required = false) String lastName) {
+        if (lastName != null)
+            return userService.findByLastName(lastName);
+        else
+            return userService.findAll();
     }
 }
