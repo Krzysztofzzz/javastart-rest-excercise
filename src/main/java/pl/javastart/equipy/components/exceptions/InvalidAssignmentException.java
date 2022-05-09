87dc52d3dfef2722ca46ff3ebc18ea0f8efc87d7
Krzysztof <krzubrycki@gmail.com>
2022-05-09 17:35:21 +0200

    repository and exception

---
+++ src/main/java/pl/javastart/equipy/components/exceptions/InvalidAssignmentException.java
@@ -0,0 +1,7 @@
+package pl.javastart.equipy.components.exceptions;
+
+public class InvalidAssignmentException extends RuntimeException {
+    public InvalidAssignmentException(String message) {
+        super(message);
+    }
+}
